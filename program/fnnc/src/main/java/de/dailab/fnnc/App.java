743c5e548cb91c1106280b3051f49e57d89ca71f
Gordon Lesti <info@gordonlesti.com>
2015-08-18 12:40:30 +0200

    init maven

---
+++ program/fnnc/src/main/java/de/dailab/fnnc/App.java
@@ -0,0 +1,23 @@
+package de.dailab.fnnc;
+
+/**
+ * Hello world!
+ *
+ */
+public final class App {
+
+  /**
+   * Hided default constructor for App.
+   */
+  private App() {
+  }
+
+  /**
+   * Main function to start the App.
+   *
+   * @param args arguments
+   */
+  public static void main(final String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ program/fnnc/src/main/java/de/dailab/fnnc/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Provides the classes necessary to compare nearest neighbor classifications.
+ */
+package de.dailab.fnnc;
