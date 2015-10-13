041a562377e4a08a8cbdeb8fbe42ce3b51ee8bf5
Gordon Lesti <info@gordonlesti.com>
2015-10-13 18:14:32 +0200

    update tests

--- program/fnnc/src/test/java/de/dailab/fnnc/search/FullSearchTest.java
+++ program/fnnc/src/test/java/de/dailab/fnnc/search/FullSearchTest.java
@@ -3,54 +3,17 @@ package de.dailab.fncc.search;
 import junit.framework.TestCase;
 import de.dailab.fnnc.search.FullSearch;
 import de.dailab.fnnc.distance.Vector;
-import de.dailab.fnnc.framework.TestDataCreator;
+import de.dailab.fnnc.command.testdata.TestData;
 
 public class FullSearchTest extends TestCase {
 
   public void testGetNearestNeighbor() {
-    double[] entries1 = {1.0, 2.0};
-    Vector vector1 = new Vector(entries1);
-    double[] entries2 = {-0.5, -3.0};
-    Vector vector2 = new Vector(entries2);
-    double[] entries3 = {-1.0, -3.0};
-    Vector vector3 = new Vector(entries3);
-
-    Vector[] vectorSet = {vector1, vector2};
+    Vector[] testVectorSet = TestData.read("target/test-classes/3-10-test");
+    Vector[] queryVectorSet = TestData.read("target/test-classes/3-1-query");
     FullSearch fsearch = new FullSearch();
-    fsearch.setVectors(vectorSet);
-    assertEquals(fsearch.getNearestNeighbor(vector3), vector2);
-  }
-
-  public void testGetNearestNeighborS1000d50() {
-    Vector[] vectors = TestDataCreator.read("target/test-classes/1000_10");
-    double[] queryEntries = {
-      937504.0,
-      538101.0,
-      469577.0,
-      87396.0,
-      570752.0,
-      539950.0,
-      650209.0,
-      190277.0,
-      -369600.0,
-      -461315.0
-    };
-    Vector queryVector = new Vector(queryEntries);
-    FullSearch fsearch = new FullSearch();
-    fsearch.setVectors(vectors);
-    double[] nnEntries = {
-      870784.0,
-      53276.0,
-      -55542.0,
-      507680.0,
-      469754.0,
-      354278.0,
-      639826.0,
-      271537.0,
-      -135787.0,
-      -362347.0
-    };
-    Vector nearestNeighbor = new Vector(nnEntries);
-    assertEquals(fsearch.getNearestNeighbor(queryVector), nearestNeighbor);
+    fsearch.setVectors(testVectorSet);
+    double[] expectedResultEntries = {1.216913797E9, -3.14664324E8, 7.88973534E8};
+    Vector expectedResultVector = new Vector(expectedResultEntries);
+    assertEquals(expectedResultVector, fsearch.getNearestNeighbor(queryVectorSet[0]));
   }
 }
