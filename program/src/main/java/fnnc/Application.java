2d778fc9e8d29dcd58814ef3d409fa302e79b60a
Gordon Lesti <info@gordonlesti.com>
2016-04-23 17:32:21 +0200

    first examples run with Application

--- program/src/main/java/fnnc/Application.java
+++ program/src/main/java/fnnc/Application.java
@@ -1,7 +1,49 @@
 package fnnc;
 
+import fnnc.algo.AnnulusMethod;
+import fnnc.algo.FullSearch;
+import fnnc.algo.OrchardsAlgorithm;
+import fnnc.model.point2ddouble.DistanceCalculator;
+
+import java.awt.geom.Point2D;
+import java.util.LinkedList;
+import java.util.Random;
+
 public class Application {
   public static void main(String[] args) {
-    System.out.println("jojo");
+    LinkedList<Point2D.Double> list = createTestData(Integer.parseInt(args[0]));
+    DistanceCalculator distanceCalculator = new DistanceCalculator();
+    AnnulusMethod<Point2D.Double> annulusMethod = new AnnulusMethod<Point2D.Double>(distanceCalculator, list);
+    System.out.println(distanceCalculator.reset());
+    FullSearch<Point2D.Double, Double> fullSearch = new FullSearch<Point2D.Double, Double>(distanceCalculator, list);
+    System.out.println(distanceCalculator.reset());
+    OrchardsAlgorithm<Point2D.Double> orchardsAlgorithm =
+        new OrchardsAlgorithm<Point2D.Double>(distanceCalculator, list);
+    System.out.println(distanceCalculator.reset());
+
+    Random rand = new Random();
+    Point2D.Double queryPoint = new Point2D.Double(rand.nextDouble(), rand.nextDouble());
+    Point2D.Double annulusMethodResult = annulusMethod.calculateNearestNeighbour(queryPoint);
+    System.out.println(distanceCalculator.reset());
+    Point2D.Double fullSearchResult = fullSearch.calculateNearestNeighbour(queryPoint);
+    System.out.println(distanceCalculator.reset());
+    Point2D.Double orchardsAlgorithmResult = orchardsAlgorithm.calculateNearestNeighbour(queryPoint);
+    System.out.println(distanceCalculator.reset());
+
+    if (fullSearchResult != annulusMethodResult || annulusMethodResult != orchardsAlgorithmResult) {
+        System.out.println("ERROR");
+    } else {
+        System.out.println("SUCCESS");
+    }
+  }
+
+  private static LinkedList<Point2D.Double> createTestData(int number)
+  {
+      LinkedList<Point2D.Double> list = new LinkedList<Point2D.Double>();
+      Random rand = new Random();
+      for (int i = 0; i < number; i++) {
+          list.add(new Point2D.Double(rand.nextDouble(), rand.nextDouble()));
+      }
+      return list;
   }
 }
--- program/src/main/java/fnnc/model/DistanceCalculator.java
+++ program/src/main/java/fnnc/model/DistanceCalculator.java
@@ -2,6 +2,20 @@ package fnnc.model;
 
 import java.lang.Comparable;
 
-public interface DistanceCalculator<T, D extends Comparable<D>> {
-  public D calculateDistance(T object1, T object2);
+public abstract class DistanceCalculator<T, D extends Comparable<D>> {
+
+  private int count = 0;
+
+  public final D calculateDistance(T object1, T object2) {
+    this.count++;
+    return internalCalculateDistance(object1, object2);
+  }
+
+  public final int reset() {
+    int result = this.count;
+    this.count = 0;
+    return result;
+  }
+
+  protected abstract D internalCalculateDistance(T object1, T object2);
 }
